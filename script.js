ac0236238370e0d36c0b60863cfe82e271175d37
Ibrahim Suleiman <ebeeraheem@gmail.com>
2023-11-27 15:25:45 +0100

    Made the page responsive

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 const isDarkMode = document.querySelector(".checkbox");
 
+const logo = document.querySelector(".logo");
+
 isDarkMode.onchange = (e) => {
   if (isDarkMode.checked === true) {
     document.documentElement.classList.remove("dark");
     document.documentElement.classList.add("light");
     window.localStorage.setItem(".checkbox", "light");
+
+    logo.src = "images/dark-logo.png";
   } else {
     document.documentElement.classList.remove("light");
     document.documentElement.classList.add("dark");
     window.localStorage.setItem(".checkbox", "dark");
+
+    logo.src = "images/light-logo.png";
   }
 };
 
 const currentMode = window.localStorage.getItem(".checkbox");
 
-// if (currentMode === "dark") {
-//   isDarkMode.checked = false;
-//   document.documentElement.classList.remove("dark");
-//   document.documentElement.classList.add("light");
-// } else {
-//   isDarkMode.checked = true;
-//   document.documentElement.classList.remove("light");
-//   document.documentElement.classList.add("dark");
-// }
-
 if (currentMode === "dark") {
   isDarkMode.checked = false;
   document.documentElement.classList.remove("light");
   document.documentElement.classList.add("dark");
+
+  logo.src = "images/light-logo.png";
+}
+
+if (currentMode === "light") {
+  isDarkMode.checked = true;
+  document.documentElement.classList.remove("dark");
+  document.documentElement.classList.add("light");
+
+  logo.src = "images/dark-logo.png";
 }
